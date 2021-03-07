8a9648a9d7b09f27b17c00e6f5a7d26caf4fee10
mim4 <milundain98@gmail.com>
2021-03-07 15:05:46 +0100

    Added description of shiny app

--- app.R
+++ app.R
@@ -3,30 +3,38 @@ library(Stat2Data)
 require(summarytools)
 data("Diamonds")
 # Define UI for application that draws a histogram
-ui <- navbarPage("My Shiny App",
+ui <- navbarPage("Marta Ilundain",
                  theme = shinytheme("united"),
-                 tabPanel("Dataset",
+                 tabPanel("Fourth Assignment: A first Shiny App",
                           fluidPage(
                             titlePanel("How to use this shiny app"),
-                            sidebarLayout(
-                              sidebarPanel(
-                                p("in this")
-                              ),
-                              mainPanel(
+                            br(),
+                            mainPanel(
+                                
+                                p("In this Shiny App we will describe the main characteristics
+                                  of the Diamonds dataset, which is contained in the Stat2Data 
+                                  package. In the first tab we can take a look to our data. There, 
+                                  we can select how many observations can be displayed by
+                                  introducing a number and then clicking the button Update Table.
+                                  In the second tab, we can see a summary of our data which has been
+                                  generated using the dfSummary function of the summarytools package. 
+                                  In this tab, we can also see how our variables are defined.
+                                  In the last tab called Plots we can see "),
                                 img(src = "diamond.png")
                               )
                             )
-                          )
-                 ),
+                          ),
+                
                  tabPanel("Data",
                           fluidPage(
-                            titlePanel("Take a look to our data"),
+                            titlePanel("Take a look to the data"),
+                            br(),
                             sidebarLayout(
                               sidebarPanel(
                                 numericInput("obs", "Number of observations to view:", 20,
                                              min = 1, max = nrow(Diamonds)),
                                 br(),
-                                submitButton("Update View")
+                                submitButton("Update Table")
                               ),
                               mainPanel(
                                   
@@ -41,7 +49,15 @@ ui <- navbarPage("My Shiny App",
                             br(),
                             sidebarLayout(
                               sidebarPanel(
-                                p("VARIABLES")
+                                p("In this dataset we have the following variables:"),
+                                br(),
+                                p("- Carat: Size of the diamond (in carats)"),
+                                p("- Color: Coded as D (most white/bright) through J"),
+                                p("- Clarity Coded as IF, VVS1, VVS2, VS1, VS2, SI1, SI2, or SI3"),
+                                p("- Depth Depth (as a percentage of diameter)"),
+                                p("- PricePerCt Price per carat"),
+                                p("- TotalPrice Price for the diamond (in dollars)")
+                                
                               ),
                               mainPanel(
                               verbatimTextOutput("summary")
